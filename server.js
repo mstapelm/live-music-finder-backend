10d4f0daeba6b27df6d7fffe736fc9266df8a161
mstapelm <mstapelm@gmail.com>
2025-03-05 21:16:28 -0600

    Fix CORS and API URL

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({ origin: "*" }));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 
 app.get("/events", async (req, res) => {
     const city = req.query.city || "New York"; // Default city if none provided
